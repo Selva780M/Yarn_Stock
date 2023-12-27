9d3684aa390d2932a15a70e1b039bdb32f895df6
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-12-27 10:47:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
         msg = f'👉 Production Bags : {bag_prd}'
         st.success(msg)
     with con2:
-        bag_des = int(df['PROD'].sum())
+        bag_des = int(df['SALES'].sum())
         #bag_des = (sum(bag_des) if bag_des else None)
         msg1 = f'👉 Despatch Bags : {bag_des}'
         st.error(msg1)    
