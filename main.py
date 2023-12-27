972327c3adf7bee2605fc08e3990b6ce308455b6
SelvaM <105146406+Selva780M@users.noreply.github.com>
2023-12-27 10:04:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import pandas as pd
 import streamlit as st
-from pandas.api.types import (
-    is_categorical_dtype,
-    is_datetime64_any_dtype,
-    is_numeric_dtype,
-    is_object_dtype,
-)
+from pandas.api.types import (is_categorical_dtype, is_datetime64_any_dtype,is_numeric_dtype,is_object_dtype,)
 
 st.title("Sulochna Cotton Spinning Mills Pvt Ltd")
 st.write("Sullerumbu - 624710")
-st.write("""Yarn Stock Report on Jun - 2019 to 2023"""
-)
+st.write("""Yarn Stock Report on Jun - 2019 to 2023""")
 
 
 def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
@@ -50,40 +44,22 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
             left.write("↳")
             # Treat columns with < 10 unique values as categorical
             if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
-                user_cat_input = right.multiselect(
-                    f"Values for {column}",
-                    df[column].unique(),
-                    default=list(df[column].unique()),
-                )
+                user_cat_input = right.multiselect(f"Values for {column}", df[column].unique(),default=list(df[column].unique()),)
                 df = df[df[column].isin(user_cat_input)]
             elif is_numeric_dtype(df[column]):
                 _min = float(df[column].min())
                 _max = float(df[column].max())
                 step = (_max - _min) / 100
-                user_num_input = right.slider(
-                    f"Values for {column}",
-                    _min,
-                    _max,
-                    (_min, _max),
-                    step=step,
-                )
+                user_num_input = right.slider(f"Values for {column}", _min, _max,(_min, _max), step=step,)
                 df = df[df[column].between(*user_num_input)]
             elif is_datetime64_any_dtype(df[column]):
-                user_date_input = right.date_input(
-                    f"Values for {column}",
-                    value=(
-                        df[column].min(),
-                        df[column].max(),
-                    ),
-                )
+                user_date_input = right.date_input(f"Values for {column}", value=(df[column].min(),df[column].max(),),)
                 if len(user_date_input) == 2:
                     user_date_input = tuple(map(pd.to_datetime, user_date_input))
                     start_date, end_date = user_date_input
                     df = df.loc[df[column].between(start_date, end_date)]
             else:
-                user_text_input = right.text_input(
-                    f"Substring or regex in {column}",
-                )
+                user_text_input = right.text_input(f"Substring or regex in {column}",)
                 if user_text_input:
                     df = df[df[column].str.contains(user_text_input)]
 
